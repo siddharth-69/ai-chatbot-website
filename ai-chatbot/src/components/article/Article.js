98443b0ff6a9753c5814e4575824d386c14e6eeb
Rajat Dutta <rajat@MacBook-Pro-2.local>
2023-11-27 02:50:56 +0530

    navbar partially completed

---
+++ ai-chatbot/src/components/article/Article.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Article() {
+  return (
+    <div>article</div>
+  )
+}
+
+export default Article
---
+++ ai-chatbot/src/components/feature/Feature.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Feature() {
+  return (
+    <div>feature</div>
+  )
+}
+
+export default Feature
